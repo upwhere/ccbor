3b3188a6119b08e1b56b7a620d015ea2e0014a4d
up here <iam@upwhere.me>
2014-03-04 17:04:36 +0000

    Basic, definite map parsing!

--- ccbor.c
+++ ccbor.c
@@ -480,6 +480,204 @@ int cbor_store_arr(struct cbor_t*storage,const uint8_t additional,const int stre
 	return EXIT_SUCCESS;
 }
 
+struct cbor_mapentry_t{
+	const struct cbor_t*const key;
+	const struct cbor_t*const value;
+};
+
+struct cbor_map_t{
+	struct cbor_t base;
+	const size_t length;
+	struct cbor_mapentry_t * const map;
+};
+
+static int cbor_store_mapentry(struct cbor_mapentry_t*storage,const uint8_t key,const int stream)
+{
+	if(storage==NULL)return 2;
+	if(key==cbor_BREAK)return 3;
+	{
+		struct cbor_t head= {
+			.major=0xFF,
+			.next=NULL,
+		};
+		struct cbor_t*pair=&head;
+
+		int store_attempt_ret;
+		uint8_t item;
+
+		if( (store_attempt_ret=cbor_store[cbor_major_of(key) ] (pair,cbor_additional_of(key),stream) ) !=EXIT_SUCCESS)return store_attempt_ret;
+
+		if(read(stream,&item,sizeof item) < (ssize_t) sizeof item)return 3;
+		// Break may not occur between a key and a value
+		if(item==cbor_BREAK)
+		{
+			store_attempt_ret=3;
+			goto free;
+		}
+
+		pair=pair->next;
+		if( (store_attempt_ret=cbor_store[cbor_major_of(item) ] (pair,cbor_additional_of(item) ,stream) ) !=EXIT_SUCCESS)goto free;
+		
+		pair=&head;
+		{
+			struct cbor_mapentry_t e= {
+				.key=(pair=pair->next),
+				.value=(pair=pair->next),
+			};
+
+			memcpy(storage,&e,sizeof*storage);
+		}
+		return EXIT_SUCCESS;free:
+		{
+			//FIXME: free whatever was stored
+			return store_attempt_ret;
+		}
+	}
+}
+
+static int cbor_store_definite_map(struct cbor_t*storage,const uint8_t additional,const int stream)
+{
+	if(storage==NULL||storage->next!=NULL)return 2;
+	{
+		const size_t length=cbor_value_uint(additional,stream);
+
+		struct cbor_mapentry_t*const map=malloc(length*(sizeof*map));
+		struct cbor_mapentry_t*map_index=map;
+		int store_attempt_ret;
+		if(map==NULL)return 1;
+
+		//Grab all map entries
+		for(size_t i=0;i<length;i++ )
+		{
+			uint8_t item;
+			if(read(stream,&item,sizeof item) < (ssize_t) sizeof item)
+			{
+				store_attempt_ret=3;
+				goto free;
+			}
+			if( (store_attempt_ret=cbor_store_mapentry(map_index,item,stream) ) !=EXIT_SUCCESS)goto free;
+			++map_index;
+		}
+
+		{
+			struct cbor_map_t m= {
+				.base= {
+					.major=cbor_major_map,
+					.next=NULL,
+				},
+				.length=length,
+				.map=map,
+			} , *fresh= malloc(sizeof*fresh);
+			if(fresh==NULL)
+			{
+				store_attempt_ret=1;
+				goto free;
+			}
+			memcpy(fresh,&m,sizeof*fresh);
+
+			storage->next=&fresh->base;
+		}
+
+		return EXIT_SUCCESS;free:
+		{
+			//FIXME: free whatever was constructed
+			return store_attempt_ret;
+		}
+	}
+}
+
+static int cbor_store_map(struct cbor_t*storage,const uint8_t additional,const int stream)
+{
+	if(storage==NULL||storage->next!=NULL)return 2;
+
+	if(additional!=cbor_additional_indefinite)
+	{
+		return cbor_store_definite_map(storage,additional,stream);
+	}
+	{
+		struct linkedlist_t{
+			struct cbor_mapentry_t*e;
+			struct linkedlist_t*next;
+		}head= {
+			.e=NULL,
+			.next=NULL,
+		} , *list_index=&head;
+
+		size_t entry_count=0;
+		int store_attempt_ret;
+
+		// Grab pairs from stream
+		while(true)
+		{
+			uint8_t item;
+
+			if(read(stream,&item,sizeof item) < (ssize_t) sizeof item)
+			{
+				store_attempt_ret=3;
+				goto free;
+			}
+
+			if(item==cbor_BREAK)break;
+
+			if( (store_attempt_ret=cbor_store_mapentry(list_index->e,item,stream) ) !=EXIT_SUCCESS)goto free;
+			
+			list_index->next=malloc(sizeof list_index->next);
+			if(list_index->e==NULL)
+			{
+				store_attempt_ret=1;
+				goto free;
+			}
+			list_index=list_index->next;
+			++entry_count;
+		}
+
+		// Copy into map.
+		{
+			size_t destination_index=0;
+			struct cbor_mapentry_t**destination=malloc((sizeof destination)*entry_count);
+			if(destination==NULL)
+			{
+				store_attempt_ret=1;
+				goto free;
+			}
+			list_index=&head;
+			while((list_index=list_index->next)!=NULL)
+			{
+				destination[destination_index++]=list_index->e;
+			}
+			{
+				struct cbor_map_t e= {
+					.base= {
+						.major=cbor_major_map,
+						.next=NULL,
+					},
+					.length=entry_count,
+					.map=*destination,
+				} , *fresh=malloc(sizeof*fresh);
+				if(fresh==NULL)
+				{
+					store_attempt_ret=1;
+					goto free;
+				}
+				memcpy(fresh,&e,sizeof*fresh);
+			}
+		}
+
+		free:
+		{
+			list_index=head.next;
+			do
+			{
+				//FIXME: deep pointers not freed.
+				if(list_index->e!=NULL)free(list_index->e);
+			}
+			while( (list_index=list_index->next) !=NULL);
+			return store_attempt_ret;
+		}
+	}
+	return EXIT_SUCCESS;
+}
+
 struct cbor_tag_t {
 	struct cbor_t base;
 	const uint64_t value;
@@ -511,13 +709,14 @@ int(*const cbor_store[cbor_major_t_max])(struct cbor_t*,const uint8_t,const int 
 	&cbor_store_bstr,
 	&cbor_store_tstr,
 	&cbor_store_arr,
-	NULL,//&cbor_store_map,
+	&cbor_store_map,
 	&cbor_store_tag,
 	NULL,//&cbor_store_flt,
 };
 
 int decode(const int stream,struct cbor_t*storage)
 {
+	if(storage==NULL)return 2;
 	int store_status=0;
 	struct cbor_t*store=storage;
 	do
--- program.c
+++ program.c
@@ -3,19 +3,24 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-void printf_cbor_t(struct cbor_t*item)
+void print_cbor_t(struct cbor_t*item,const int nindent)
 {
+	char indent[nindent+1];
+	for(int i=0;i<nindent;i++ )
+		indent[i]= ' ';
+	indent[nindent]= '\0';
+
 	switch(item->major>>5)
 	{
 		default:
 		case 0:
-			printf( "\t%d (+int)\t:: %llu\n",item->major>>5,(long long unsigned int)((struct cbor_uint_t*)item)->value);
+			printf( "\t%s%d (+int)\t%s:: %llu\n",indent,item->major>>5,indent, (long long unsigned int) ((struct cbor_uint_t*)item)->value);
 			break;
 		case 1:
-			printf( "\t%d (-int)\t:: 1-%llu\n (= -%llu?)\n", item->major>>5, ((unsigned long long int)1)-(unsigned long long int)((struct cbor_nint_t*)item)->nvalue,(unsigned long long int)((struct cbor_nint_t*)item)->nvalue );
+			printf( "\t%s%d (-int)\t%s:: 1-%llu\n (= -%llu?)\n",indent,item->major>>5,indent, ((unsigned long long int)1)-(unsigned long long int)((struct cbor_nint_t*)item)->nvalue,(unsigned long long int)((struct cbor_nint_t*)item)->nvalue );
 			break;
 		case 2:
-			printf( "\t%d (bstr)\t:: ", item->major>>5);
+			printf( "\t%s%d (bstr)\t%s:: ",indent,item->major>>5,indent);
 			for(size_t i=0;i<((struct cbor_bstr_t*)item)->length;i++)
 			{
 				printf( "%02x ", ((struct cbor_bstr_t*)item)->string[i] );
@@ -23,20 +28,35 @@ void printf_cbor_t(struct cbor_t*item)
 			putchar( '\n' );
 			break;
 		case 3:
-			printf( "\t%d (tstr)\t:: %s\n", item->major>>5, ((struct cbor_tstr_t*)item)->string);
+			printf( "\t%s%d (tstr)\t%s:: %s\n",indent,item->major>>5,indent, ((struct cbor_tstr_t*)item)->string);
 			break;
-		case 4:;
+		case 4:
+		{
 			struct cbor_t**x;
-			printf( "\t%d (arr )\t:: [\n", item->major>>5);
+			printf( "\t%s%d (*arr)\t%s:: [\n",indent,item->major>>5,indent);
 			x=((struct cbor_arr_t*)item)->array;
 			for(size_t i=0;i<((struct cbor_arr_t*)item)->length;i++)
 			{
-				printf_cbor_t(x[i]);
+				print_cbor_t(x[i],nindent+1);
 			}
-			printf( "\t       \t   ]\n" );
+			printf( "\t%s        \t%s   ]\n",indent,indent);
 			break;
+		}
+		case 5:
+		{
+			struct cbor_mapentry_t*const x=((struct cbor_map_t*)item)->map;
+			printf( "\t%s%d (*map)\t%s:: [\n",indent,item->major>>5,indent);
+			for(size_t i=0;i<((struct cbor_map_t*)item)->length;i++)
+			{
+				print_cbor_t((struct cbor_t*)x[i].key,nindent+1);
+				print_cbor_t((struct cbor_t*)x[i].value,nindent+1);
+				printf( "\t%s        \t%s---\n",indent,indent);
+				
+			}
+			printf( "\t%s        \t%s   ]\n",indent,indent);
+		}	
 		case 6:
-			printf( "\t%d (tag )\t:: %llu\n",item->major>>5,(long long unsigned int)((struct cbor_uint_t*)item)->value);
+			printf( "\t%s%d (tag )\t:: %llu\n",indent,item->major>>5,(long long unsigned int)((struct cbor_uint_t*)item)->value);
 			break;
 	}
 }
@@ -48,7 +68,7 @@ int main(void)
 	px=x.next;
 	while(px!=NULL)
 	{
-		printf_cbor_t(px);
+		print_cbor_t(px,0);
 		px=px->next;
 	}
 }
